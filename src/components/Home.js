56118ff387478a38d47c2b94b760d8d28d65e009
Pella0 <pellattiero.ju@gmail.com>
2020-11-19 16:50:15 +0100

    ajout d'un banner dans home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import AboutUs from './AboutUs';
-
+import Banner from './Banner';
 
 const Home = () => {
   return (
     <div>
-
-      <AboutUs />
+      <Banner />
     </div>
   );
 };
