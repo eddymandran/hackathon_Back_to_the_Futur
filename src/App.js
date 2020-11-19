e4c2cdb54e16a50fa60f4c3438a5051ee9d881af
eddymandran <dev.eddy.mandran@gmail.com>
2020-11-19 17:36:52 +0100

    We can see the map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import MapView from './components/MapView';
 
 function App() {
-  return <div className='App'>Hackathon G4</div>;
+  return (
+    <div className='App'>
+      Hackathon G4
+      <MapView />
+    </div>
+  );
 }
 
 export default App;
