6e94588342875a457b0691cc39a9188fbf727cd5
Pella0 <pellattiero.ju@gmail.com>
2020-11-19 21:01:26 +0100

    ajout timer

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../styles/Home.css';
 import Banner from './Banner';
+import Timer from './Timer';
 
 export default function Home() {
   return (
     <div>
       {' '}
       <Banner />
+      <Timer />
     </div>
   );
 }
