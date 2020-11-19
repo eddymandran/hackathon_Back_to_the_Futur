f078a1d6a360f1b1b4f021c938f3c4c3306fb93f
Pella0 <pellattiero.ju@gmail.com>
2020-11-19 21:19:30 +0100

    ajout Form dans component Info

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AboutUs from './AboutUs';
+import Form from './Form';
 
 function Info() {
   return (
     <div>
       <div className='info'>
         <AboutUs />
+        <Form />
       </div>
     </div>
   );
