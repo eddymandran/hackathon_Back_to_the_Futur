62a7f4f26c8607f3bc1c661f3c8ec6f41a0bad80
eddymandran <dev.eddy.mandran@gmail.com>
2020-11-19 19:35:31 +0100

    ajout du fichier json

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -8,15 +8,15 @@ export default function MapView() {
     <div>
       <MapContainer
         className='MapViewContainer'
-        center={[51.505, -0.09]}
-        zoom={13}
+        center={[45.746463, 4.827158]}
+        zoom={15}
         scrollWheelZoom={false}
       >
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={[45.746463, 4.827158]}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
