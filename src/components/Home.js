958358bb4c0ad8e6f523158bae235519841d343e
SaKara69 <sabrina.karakog@gmail.com>
2020-11-19 22:17:21 +0100

    ajout logo barre de Nav

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import '../styles/Home.css';
 import Banner from './Banner';
-import MapView from './components/MapView';
+import MapView from './MapView';
 
 export default function Home() {
   return (
     <div>
-      {' '}
       <Banner />
       <MapView />
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,14 +2,19 @@ import React from 'react';
 import '../styles/NavBar.css';
 import BurgerMenu from './Burgermenu';
 import { Link } from 'react-router-dom';
+import logo from '../img/logo.png';
 
 export default function NavBar() {
   return (
     <header>
+      <Link to='/'>
+        <img src={logo} alt='hat' className='logo' />
+      </Link>
       <nav>
         <ul>
+          <li></li>
           <li>
-            <Link to='/'>Home</Link>
+            <Link to='/'>Occire Elites</Link>
           </li>
           <li>
             <Link to='/Targets'>Targets</Link>
@@ -19,6 +24,7 @@ export default function NavBar() {
           </li>
         </ul>
       </nav>
+
       <BurgerMenu />
     </header>
   );
